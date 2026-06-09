d6c4eb1bea77f385c5887f8a8181aae708bb056f
Ahmed Almasri <s4039955@studnt.rmit.edu.au>
2026-06-09 14:43:31 +1000

    full Pipeline

---
+++ visualisation/potree_vis/pointclouds/riga/city/cloud.js
@@ -0,0 +1,83 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 649333,
+    "boundingBox": {
+        "lx": -56.95,
+        "ly": -52.97,
+        "lz": -4.8500000000000009,
+        "ux": 55.099999999999997,
+        "uy": 59.08,
+        "uz": 107.2
+    },
+    "tightBoundingBox": {
+        "lx": -56.95,
+        "ly": -52.97,
+        "lz": -4.8500000000000009,
+        "ux": 37.74,
+        "uy": 59.08,
+        "uz": 1.1500000000000002
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "segment_id",
+            "size": 4,
+            "elements": 1,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "segment_overall_width",
+            "size": 4,
+            "elements": 1,
+            "elementSize": 4,
+            "type": "float",
+            "description": ""
+        },
+        {
+            "name": "segment_usable_width",
+            "size": 4,
+            "elements": 1,
+            "elementSize": 4,
+            "type": "float",
+            "description": ""
+        },
+        {
+            "name": "segment_slope_percent",
+            "size": 4,
+            "elements": 1,
+            "elementSize": 4,
+            "type": "float",
+            "description": ""
+        }
+    ],
+    "spacing": 0.9703814387321472,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
